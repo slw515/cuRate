e314360b44bfc4ee65f1add72157214bd95ba46f
slw515 <slw515@nyu.edu>
2021-10-24 14:42:38 +0800

    edit index.js file

--- index.js
+++ index.js
@@ -18,12 +18,11 @@ const server = new ApolloServer({
 mongoose
   .connect(MONGODB, { useNewUrlParser: true })
   .then(() => {
-    console.log("db success");
     return server.listen({ port: PORT });
   })
   .then(res => {
-    console.log("server started: " + res.url);
+    // console.log("server started: " + res.url);
   })
   .catch(err => {
-    console.log(err);
+    // console.log(err);
   });
